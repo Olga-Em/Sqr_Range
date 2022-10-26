997e55e740aa3bfb17c512e6f34767058c8e7b70
Olga Em <olga.em020@gmail.com>
2022-10-26 13:39:05 +0700

    change pom

--- src/test/java/ru/netology/sqr/SQRServeces/SqrtInRangeTest.java
+++ src/test/java/ru/netology/sqr/SQRServeces/SqrtInRangeTest.java
@@ -2,8 +2,6 @@ package ru.netology.sqr.SQRServeces;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class SqrtInRangeTest {
 
